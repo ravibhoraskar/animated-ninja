e10f18483c23db6bea693987e1eeb62353739ef2
Ravi Bhoraskar <rav@fb.com>
2014-09-22 00:20:53 -0700

    Merging Pradeep's stuff

--- ninja/src/main/java/com/ninja/animated/SuggestionFragment.java
+++ ninja/src/main/java/com/ninja/animated/SuggestionFragment.java
@@ -2,13 +2,9 @@ package com.ninja.animated;
 
 
 
-import android.support.v4.app.FragmentManager;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
